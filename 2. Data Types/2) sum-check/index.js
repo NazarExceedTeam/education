131fc217a819ad02c6e07965319461479334cdd8
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-05 16:16:12 +0300

    sum-check

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,5 +1,12 @@
 function sumCheck(a, b) {
-  // Напиши свой код здесь
+        if (a === isNaN || b === isNaN){
+            return NaN;
+        }
+        if (typeof a === 'number' && typeof b === 'number'){
+            return  a + b;
+        } else {
+            throw "Wrong arguments type!";
+        }
 }
 
 window.sumCheck = sumCheck;
