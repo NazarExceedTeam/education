a864339da011b79099174fbc2c7bf66838a044b0
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-05 10:22:29 +0300

    basic-calc

--- 1. Basic/3) basic-calc/index.js
+++ 1. Basic/3) basic-calc/index.js
@@ -1,5 +1,10 @@
 function basicCalc(operation, a, b) {
-  // Напиши свой код здесь
+  switch (operation) {
+      case '+': return a+b; break;
+      case '-': return a-b; break;
+      case '*': return a*b; break;
+      case '/': return a/b; break;
+  }
 }
 
 window.basicCalc = basicCalc;
