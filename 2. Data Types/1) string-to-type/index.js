2ed68db5393feb4a511cc41a1e9fab0df26dd78c
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-05 14:29:31 +0300

    string to type

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,5 +1,22 @@
 function stringToType(str) {
-  // Напиши свой код здесь
+    let result = str;
+    if (result === "undefined"){
+        return undefined;
+    } else if (result === "null"){
+        return null;
+    } else if(result === "") {
+        return "";
+    } else if(result === "false") {
+        return false;
+    } else if(result === "true") {
+        return true;
+    } else {
+        if (parseInt(result, 10)==result) {
+            return parseInt(result, 10);
+        } else {
+            return result;
+        }
+    }
 };
 
 window.stringToType = stringToType;
