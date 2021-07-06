9fd2baf8e53d51386a536c78aa702ec7a0134d1d
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-06 12:38:18 +0300

    find pair

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,5 +1,19 @@
 function findPair(arr) {
-  // Напиши свой код здесь
+    let check_arr = [];
+    for (let i = 0; i < arr.length; i++){
+        if (check_arr.length === 0){
+            check_arr.push(arr[i]);
+            continue;
+        } else {
+            for (let j = 0; j < check_arr.length; j++){
+                if (arr[i] === check_arr[j]){
+                    return arr[i];
+                }
+            }
+            check_arr.push(arr[i]);
+        }
+    }
+    return null;
 };
 
 window.findPair = findPair;
