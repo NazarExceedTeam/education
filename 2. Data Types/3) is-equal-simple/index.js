5f915fa59c20f46633328676eeddf0c12575d9c0
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-06 17:55:36 +0300

    промежуточное решение задач is equal simple (array) и query object (data types)

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,10 +1,47 @@
+window.isEqual = isEqual;
+
 function isEqual(a, b) {
 
+    if (a === b){
+        return true;
+    }
+    if ((a === "{}") && (b === "{}")){
+        return true;
+    }
+    if ((typeof a == "object") && (typeof b == "object" )){
+        if (Object.keys(a).length !== Object.keys(b).length){
+            return false;
+        } else {
+            if (Object.entries(a)===Object.entries(b)){
+                return true;
+            }
+            let a_keys = [];
+            let b_keys = [];
+            let a_values = [];
+            let b_values = [];
+            a_keys = Object.keys(a);
+            b_keys = Object.keys(b);
+            a_values = Object.values(a);
+            b_values = Object.values(b);
+            for (let i in a_keys.length){
+                for (let j in b_keys.length){
+                    if ((a_keys[i] === b_keys[j]) && (a_values[i] === b_values[j])){
+                        return true;
+                    }
+                }
+            }
+
+        }
+    }
 
 
-    return JSON.stringify(a) == JSON.stringify(b);
+    //Object.is(a ,b);
+    //JSON.stringify(a) == JSON.stringify(b);
+    // return true;
+    // return Object.is(a ,b);
+
+
+
 }
-
-window.isEqual = isEqual;
 
 export default isEqual;
--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,5 +1,25 @@
 function queryToObject(query) {
-  // Напиши свой код здесь
+    console.log(query.split('&').split('&'))
+    let str=query;
+    if (str.isArray()){
+        console.log("is array - true");
+        let obj='';
+        for(let i = 0; i < str.length; i++){
+            console.log("inside for, i="+i);
+            switch (str[i]) {
+                case '?': continue;
+                case '&': obj=+', '; continue;
+                case '=': obj=+': '; continue;
+                default: switch (str[i]){
+                    case (!Number.isNaN(str[i]) && (str[i + 1] === ',') || (str[i-1] === ':')): obj=+str[i];
+                    default: obj=+str[i];
+                }
+            }
+        }
+        console.log('{' + obj + '}');
+    }
+
+    return stringToType(str);
 };
 
 window.queryToObject = queryToObject;
