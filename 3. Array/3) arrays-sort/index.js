78fd08f21b5210ca1598d583b77949fff95b56e0
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-06 16:09:49 +0300

    arrays sort

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,5 +1,40 @@
 function arraysSort(arr) {
-  // Напиши свой код здесь
+    let a=arr.slice();
+
+    if (a.length <= 1){
+        return a;
+    }
+    console.log(a)
+    let sorted = false;
+    let i = 0;
+    while (true){
+        if (i === a.length-1){
+            i = 0;
+            if (sorted){
+                break;
+            }
+            sorted = true;
+        }
+        if (max(a[i]) > max(a[i+1])){
+            sorted=false;
+            let cache=a[i];
+            a[i]=a[i+1];
+            a[i+1]=cache;
+        }
+        i++;
+    }
+    console.log(a);
+    return a;
+
+    function max(a){
+        let max=a[0];
+        for (let i = 1; i < a.length; i++){
+            if (max < a[i]){
+                max = a[i];
+            }
+        }
+        return max;
+    }
 };
 
 window.arraysSort = arraysSort;
