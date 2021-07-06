e1e760d81e76265bf4aa724059964264e6160daf
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-06 12:22:15 +0300

    full-sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,21 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+    if (args.length === 0){
+        return 0;
+    }
+    let summ = 0;
+    console.log(args)
+    for (let i=0; i < args.length; i++){
+        if (Number.isNaN(args[i])){
+            return NaN;
+        }
+        console.log(typeof args[i])
+        if (typeof args[i] === "number"){
+            summ = summ + args[i];
+        } else {
+            throw "Wrong arguments type!";
+        }
+  }
+    return summ;
 }
 
 window.fullSum = fullSum;
