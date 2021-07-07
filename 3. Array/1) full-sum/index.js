b2910b83883350c84ba0cc11a538cb5a0a9bf686
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-07 12:40:28 +0300

    оптимизация full sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,21 +1,13 @@
 function fullSum(...args) {
-    if (args.length === 0){
-        return 0;
+    let sum = 0;
+
+    for (let i = 0; i < args.length; i++) {
+        if (typeof args[i] !== "number") {
+            throw "Wrong arguments type!"
+        }
+        sum = sum + args[i];
     }
-    let summ = 0;
-    console.log(args)
-    for (let i=0; i < args.length; i++){
-        if (Number.isNaN(args[i])){
-            return NaN;
-        }
-        console.log(typeof args[i])
-        if (typeof args[i] === "number"){
-            summ = summ + args[i];
-        } else {
-            throw "Wrong arguments type!";
-        }
-  }
-    return summ;
+    return sum;
 }
 
 window.fullSum = fullSum;
