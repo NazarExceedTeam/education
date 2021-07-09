03a8d787c6fb374d75c903244492fc9d9960f9f5
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-09 18:03:48 +0300

    row zero

--- 5. Others/6) row-zero/index.js
+++ 5. Others/6) row-zero/index.js
@@ -1,5 +1,18 @@
 function rowZero(str) {
-  // Напиши свой код здесь
+    let strArray = str.split('');
+    let max = 0;
+    let current_max = 0;
+    strArray.forEach((currVal) => {
+        if (currVal == 0) {
+            current_max++;
+        } else {
+            current_max = 0;
+        }
+        if (current_max > max) {
+            max++;
+        }
+    } );
+    return max;
 }
 
 window.rowZero = rowZero;
