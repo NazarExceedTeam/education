e09165c84b8526bbdc5c477c17e257033e7729c9
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-09 17:36:04 +0300

    async sum update

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,14 +1,8 @@
 function asyncSum(a, b) {
     let prom = new Promise((resolve, reject) => {
-
-        let bool = (Number.isInteger(a) || isNaN(a)) && Number.isInteger(b);
-        console.log(`${a} and ${b} isNaN = ${bool}`);
-        setTimeout(() => {if (bool){
-                resolve (a+b);
-            } else {
-                reject(new Error("Error"));
-            }
-        }, 1000);
+        if (!(Number.isInteger(a) || isNaN(a)) && Number.isInteger(b)) reject(new Error("Error"));
+        setTimeout(() => {
+                resolve (a+b)}, 1000);
     })
     return prom;
 }
