5dfb9ca2672ffad9595dca93f3b459176e913511
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-09 17:36:36 +0300

    xhr get

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,7 +1,35 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest()
+        xhr.open('GET', url, true)
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300){
+                resolve(xhr.response);
+            }
+            reject(xhr.response)
+        };
+        xhr.send()
+    })
 }
 
 window.xhrGet = xhrGet;
 
 export default xhrGet;
+
+
+// console.log("Promise has been created!")
+// let servConn = new XMLHttpRequest();
+// console.log(`Connecting to server... URL: ${url}`);
+// servConn.open("GET", url, true);
+// console.log("Connection opened!")
+// servConn.send();
+// console.log("Request sent!")
+// servConn.load();
+// console.log(`URL =  "${url}"`);
+// console.log(`Status =  "${servConn.statusText}"`);
+// console.log(`Response = "${servConn.responseText}"`);
+// if (servConn.status >= 200 && servConn.status < 300) resolve (servConn);
+// reject (new Error(servConn.statusText));
+// console.log(prom);
