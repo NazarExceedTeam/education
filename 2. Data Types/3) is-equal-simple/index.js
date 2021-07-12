05b8052aaaa2552c90e2411412fb36fad66a056e
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-12 17:20:57 +0300

    is equal simple

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,38 +1,34 @@
 window.isEqual = isEqual;
 
 function isEqual(a, b) {
-
     if (a === b){
         return true;
     }
     if ((a === "{}") && (b === "{}")){
         return true;
     }
+    if (a === null){
+        return b === null;
+    }
     if ((typeof a == "object") && (typeof b == "object" )){
         if (Object.keys(a).length !== Object.keys(b).length){
             return false;
         } else {
-            if (Object.entries(a)===Object.entries(b)){
+            if ((Object.values(a)[0] === undefined) && (Object.values(b)[0] === undefined)){
+                return Object.keys(a)[0] === Object.keys(b)[0];
+            }
+            if (Object.entries(a) === Object.entries(b)){
                 return true;
             }
-            let a_keys = [];
-            let b_keys = [];
-            let a_values = [];
-            let b_values = [];
-            a_keys = Object.keys(a);
-            b_keys = Object.keys(b);
-            a_values = Object.values(a);
-            b_values = Object.values(b);
-            for (let i in a_keys.length){
-                for (let j in b_keys.length){
-                    if ((a_keys[i] === b_keys[j]) && (a_values[i] === b_values[j])){
-                        return true;
-                    }
-                }
+            if ((Object.keys(a)[0] === Object.keys(b)[1]) && (Object.keys(a)[1] === Object.keys(b)[0])){
+                return ((Object.values(a)[0] === Object.values(b)[1]) && (Object.values(a)[1] === Object.values(b)[0]));
             }
-
         }
     }
+
+    return JSON.stringify(a) == JSON.stringify(b);
 }
+
+window.isEqual = isEqual;
 
 export default isEqual;
