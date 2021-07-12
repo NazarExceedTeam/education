afb59f00ead5d9bf013edd27d50b25b9d31278c4
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-12 11:08:04 +0300

    is equals advances

--- 5. Others/7) is-equal-advances/index.js
+++ 5. Others/7) is-equal-advances/index.js
@@ -1,5 +1,29 @@
 function isEqualAdvances(a, b) {
-  // Напиши свой код здесь
+    if (a === b) {
+        return true;
+    }
+    if ((a === null) || (b === null)){
+        return a===b;
+    }
+    if ((a === undefined) || (b === undefined)){
+        return Object.values(a)[0] === Object.values(b)[0];
+    }
+    if ((typeof a == "object") && (typeof b == "object" )){
+        if (Object.keys(a).length !== Object.keys(b).length){
+            return false;
+        } else {
+            if (Object.keys(a)[0] === Object.keys(b)[0]){
+                if (typeof Object.values(a)[0] === "object" && Object.values(a)[0] !== null){
+                    a = Object.values(a)[0];
+                    b = Object.values(b)[0];
+                }
+                if ((Object.values(a)[0] === Object.values(b)[0])){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
 
 window.isEqualAdvances = isEqualAdvances;
